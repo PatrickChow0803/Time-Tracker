37ba5d4d541a2951d83651435645324a6d73eeba
patrickchow0803 <patrickchow0803@gmail.com>
2020-08-17 21:41:35 -0400

    Added more boxes to be used as a blue print for buttons and added proper spacing.

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -8,19 +8,37 @@ class SignInPage extends StatelessWidget {
         title: Center(child: Text("Time Tracker")),
         elevation: 2.0,
       ),
-      body: Container(
-        color: Colors.yellow,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              color: Colors.orange,
-              child: SizedBox(
-                height: 100.0,
-              ),
-            )
-          ],
-        ),
+      body: _buildContainer(),
+    );
+  }
+
+  Widget _buildContainer() {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            color: Colors.orange,
+            child: SizedBox(
+              height: 100.0,
+            ),
+          ),
+          SizedBox(height: 8.0),
+          Container(
+            color: Colors.red,
+            child: SizedBox(
+              height: 100.0,
+            ),
+          ),
+          SizedBox(height: 8.0),
+          Container(
+            color: Colors.purple,
+            child: SizedBox(
+              height: 100.0,
+            ),
+          ),
+        ],
       ),
     );
   }
