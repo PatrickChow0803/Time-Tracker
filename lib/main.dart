d18b01f5058601e2016c00007b1e5eff1446bd8a
patrickchow0803 <patrickchow0803@gmail.com>
2020-08-17 21:18:09 -0400

    Added a sign in page with a scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/app/sign_in/sign_in_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,11 +9,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Time Tracker",
-      theme: ThemeData(primarySwatch: Colors.indigo),
-      home: Container(
-        color: Colors.white,
-      ),
-    );
+        title: "Time Tracker",
+        theme: ThemeData(primarySwatch: Colors.indigo),
+        home: SignInPage());
   }
 }
