638704c5ccc2b6e167dd2202b78ea49bdcdc3590
patrickchow0803 <patrickchow0803@gmail.com>
2020-08-28 14:33:51 -0400

    Added more buttons to be used for logging in

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -31,7 +31,8 @@ class SignInPage extends StatelessWidget {
               fontWeight: FontWeight.w600,
             ),
           ),
-          SizedBox(height: 8.0),
+          SizedBox(height: 48.0),
+//          Now using a SignInButton (Which extends from CustomRaisedButton) instead
 //          CustomRaisedButton(
 //            child: Text(
 //              'Sign in with Google',
@@ -48,6 +49,32 @@ class SignInPage extends StatelessWidget {
             text: 'Sign in with Google',
             textColor: Colors.black87,
             color: Colors.white,
+            onPressed: () {},
+          ),
+          SizedBox(height: 16.0),
+          SignInButton(
+            text: 'Sign in with Facebook',
+            textColor: Colors.white,
+            color: Color(0xFF334D92),
+            onPressed: () {},
+          ),
+          SizedBox(height: 16.0),
+          SignInButton(
+            text: 'Sign in with Email',
+            textColor: Colors.white,
+            color: Colors.teal[700],
+            onPressed: () {},
+          ),
+          SizedBox(height: 16.0),
+          Text(
+            'or',
+            style: TextStyle(fontSize: 14.0, color: Colors.black87),
+            textAlign: TextAlign.center,
+          ),
+          SignInButton(
+            text: 'Go anonymous',
+            textColor: Colors.black,
+            color: Colors.lime[300],
             onPressed: () {},
           ),
         ],
