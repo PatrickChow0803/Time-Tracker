58add7a04cdd080045c74569c1cf69cc5ef641aa
patrickchow0803 <patrickchow0803@gmail.com>
2020-12-07 12:46:02 -0500

    Added an User class to decouple Firebase Auth information

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,23 +1,39 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
+// This AppUser class is used to decouple the extra information given from Firebase Auth
+// For example, this AppUser class only contains the Uid
+// While a FirebaseAuth.instance.currentUser contains a LOT more information than just the Uid
+class AppUser {
+  final String Uid;
+  AppUser({this.Uid});
+}
+
 abstract class AuthBase {
-  User currentUser();
-  Future<User> signInAnonymously();
+  AppUser currentUser();
+  Future<AppUser> signInAnonymously();
   Future<void> signOut();
 }
 
-class Auth implements AuthBase{
+class Auth implements AuthBase {
   final _firebaseAuth = FirebaseAuth.instance;
 
-  @override
-  User currentUser() {
-    return _firebaseAuth.currentUser;
+  AppUser _userFromFirebase(User user) {
+    if (user == null) {
+      return null;
+    }
+    return AppUser(Uid: user.uid);
   }
 
   @override
-  Future<User> signInAnonymously() async {
+  AppUser currentUser() {
+    final user = _firebaseAuth.currentUser;
+    return _userFromFirebase(user);
+  }
+
+  @override
+  Future<AppUser> signInAnonymously() async {
     final UserCredential user = await _firebaseAuth.signInAnonymously();
-    return user.user;
+    return _userFromFirebase(user.user);
   }
 
   @override
