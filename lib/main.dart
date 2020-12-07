a1e8b39b9d0dd1b163918d2284747547066887b6
patrickchow0803 <patrickchow0803@gmail.com>
2020-12-06 20:53:23 -0500

    Updated project to work with the new Firebase requirements

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:time_tracker_flutter_course/app/landing_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
