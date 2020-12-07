b1f2fc9d8a032d9a7bf3716a4a9fd69745d03c5e
patrickchow0803 <patrickchow0803@gmail.com>
2020-12-07 12:27:59 -0500

    Added an abstract class for the Firebase Auth methods

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,17 +1,26 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
-class Auth {
+abstract class AuthBase {
+  User currentUser();
+  Future<User> signInAnonymously();
+  Future<void> signOut();
+}
+
+class Auth implements AuthBase{
   final _firebaseAuth = FirebaseAuth.instance;
 
+  @override
   User currentUser() {
     return _firebaseAuth.currentUser;
   }
 
+  @override
   Future<User> signInAnonymously() async {
     final UserCredential user = await _firebaseAuth.signInAnonymously();
     return user.user;
   }
 
+  @override
   Future<void> signOut() async {
     await _firebaseAuth.signOut();
   }
